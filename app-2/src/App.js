4fd16b3a5d3d400dac7b3fc70b31ac843f0e40c8
Trevor <alutiiq@comcast.net>
2020-07-22 14:36:08 -0700

    slow progress

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  constructor(){
+    super()
+
+    this.state = {
+
+      names: ['Trevor', 'Tessa', 'Ellie', 'Ashley', 'Trenton'] 
+
+    }
+  }
+
+  render(){
+    let displayNames = this.state.names.map((elem, ind)=>{
+
+      return <h2> key {ind} > {elem}</h2>   
+
+    })
+
+    return <div className='App'>{displayNames}</div>   
+
+  }
 }
+
 
 export default App;
--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      filteredName: '',
+      names: ['Trevor', 'Tessa', 'Ellie', 'Ashley', 'Trenton']
+    }
+  }
+
+  handleChange(filter){
+    this.setState({
+      filteredName: filter
+    })
+  }
+
+
+  render(){
+
+    let displayNames = this.state.names.filter((elem, ind) => {
+      return elem.includes(this.state.filteredName)
+    })
+
+    .map((elem, ind)=>{
+      return <h2> Key = {ind} > {elem}</h2>
+    })
+    
+    return (
+      <div className="App">
+        <input onChange={e => this.handleChange(e.target.value)} type="text" />
+        {displayNames}
+      </div>
+    );
+  }
+
+
+
 }
 
 export default App;
